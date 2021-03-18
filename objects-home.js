671493269720bd5d449bad5eefd7238b83e63df5
ErikMarkosyan <er001.em@gmail.com>
2021-03-18 17:08:04 +0400

    an object vas added and ran by for loop

--- objects-home.js
+++ objects-home.js
@@ -1,5 +1,5 @@
 let user1 = new Object(); // "object constructor" syntax
-let user2 = {};  // "object literal" syntax
+// let carMercedes = {};  // "object literal" syntax
 
 // օբյեկտների մեջ key: value ավելացնելու 2ձեւ կա։
 
@@ -8,9 +8,39 @@ let user2 = {};  // "object literal" syntax
 user1.name = "Mike";
 user1.age = 54;
 user1.gender = "male";
-console.log(user1);
+// console.log(user1);
 
 //  2. user1 ին տալիս ենք ազգանուն, ադմին
 
 user1['surname'] = "Tigranyan";
 user1['isAdmin'] = false;
+
+// կարանք փոխենք key-ի value-ն
+
+user1['age'] = 17;
+
+// կարանք ջնջենք isAdmin key-ն իրա value-ով
+
+delete user1.isAdmin;
+// կամ delete user["isAdmin"];
+
+let carMercedes = {
+  make: "Mercedes-Benz",
+  model: "S-Class",
+  body: "W221",
+  year: 2010,
+  color: "Black",
+  engine: "V8",
+  hp: "435 Horse Powers",
+};
+// console.log("<=>", carMercedes);
+
+// ստուգումը թե արդյոք կա key value օբյեկտի մեջ անում ենք հետեւյալ ձեւով
+
+console.log("make" in carMercedes ); //boolean  true or false
+console.log("fuel" in carMercedes); // false
+
+for (let key in carMercedes){  // loop
+  // console.log(key); // լոգ ա անելու օբյեկտի մեջի key-երը
+  console.log(carMercedes[key]); // լոգ ա անելու օբյեկտի մեջի value-ները 
+};
